7a4d37923954aeb1074275abd31876d0e28d5cb9
Sendiko <sarangtawon897@gmail.com>
2024-07-23 18:33:49 +0700

    enc(add): data holder for any other data

--- selector-component/src/main/java/com/sendiko/selector_component/SelectorData.kt
+++ selector-component/src/main/java/com/sendiko/selector_component/SelectorData.kt
@@ -1,6 +1,18 @@
 package com.sendiko.selector_component
 
+/**
+ *
+ * Data class for Sendiko's custom selector component
+ *
+ * @param index to uniquely identified data.
+ * @param label for the text that will be displayed.
+ * @param data holder for any additional data to be linked.
+ *
+ * @author Sendiko
+ *
+ * */
 data class SelectorData(
     val index: Int,
     val label: String,
+    val data: Any? = null,
 )
