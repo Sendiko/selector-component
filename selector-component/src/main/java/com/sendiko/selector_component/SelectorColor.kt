bc397552767b425b044c4a3d1a0f9ce86b9d3c36
Sendiko <sarangtawon897@gmail.com>
2024-07-23 18:32:39 +0700

    change to nullable

--- selector-component/src/main/java/com/sendiko/selector_component/SelectorColor.kt
+++ selector-component/src/main/java/com/sendiko/selector_component/SelectorColor.kt
@@ -4,7 +4,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
 
 data class SelectorColor(
-    val buttonColor: Color = Color.DarkGray,
-    val textColor: Color = Color.White,
-    val surfaceColor: Color = Color.LightGray,
+    val buttonColor: Color? = null,
+    val textColor: Color? = null,
+    val surfaceColor: Color? = null,
 )
